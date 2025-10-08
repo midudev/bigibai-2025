e487d4c16affebea5c33523289bc9bfc5edda53a
Sheñey <sheneyby2010@gmail.com>
2025-10-08 17:32:02 -0600

    refactor: Uso de alias @ para ./src/* en Astro.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'node:path';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
   }
 });
