f6fff7a0db9eb3b320d2390df3df50444b286988
midudev <miduga@gmail.com>
2025-10-10 17:15:05 +0200

    Add site URL configuration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.bigibai.com',
   adapter: vercel(),
   integrations: [sitemap()],
   vite: {
