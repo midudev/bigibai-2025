5aa6c5da2db410f7042d2f0a18222640440554bc
midudev <miduga@gmail.com>
2025-10-15 11:27:10 +0200

    feat(supabase): add support for session persistence and admin client configuration

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,19 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL
-const supabaseKey = import.meta.env.SUPABASE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseAdminKey = import.meta.env.SUPABASE_ADMIN_KEY
 
+// Cliente para el servidor (sin persistencia de sesión)
 export const supabase = createClient(supabaseUrl, supabaseKey)
+
+// Cliente para el cliente (con persistencia de sesión)
+export const supabaseClient = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+})
+
+export const supabaseAdmin = createClient(supabaseUrl, supabaseAdminKey)
