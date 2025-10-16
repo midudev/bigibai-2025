3641f28ab8fd7ed8d2ce29c8a793d7aac95cd629
midudev <miduga@gmail.com>
2025-10-16 12:45:35 +0200

    feat(supabase): create supabase-admin client and update environment variable usage

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,19 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-const supabaseAdminKey = import.meta.env.SUPABASE_ADMIN_KEY
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY
 
-// Cliente para el servidor (sin persistencia de sesión)
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
-// Cliente para el cliente (con persistencia de sesión)
-export const supabaseClient = createClient(supabaseUrl, supabaseKey, {
+// Cliente para el cliente (con persistencia de sesión y PKCE)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true
-  }
+    flowType: "pkce"
+  },
 })
-
-export const supabaseAdmin = createClient(supabaseUrl, supabaseAdminKey)
