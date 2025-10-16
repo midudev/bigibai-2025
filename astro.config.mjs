7eefdb24749315a33cad51991e8edaa44134459f
midudev <miduga@gmail.com>
2025-10-16 12:44:36 +0200

    chore(config): standardize import statements and update output configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,25 @@
 // @ts-check
-import path from 'node:path';
+import path from "node:path";
 
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.bigibai.com',
+  site: "https://www.bigibai.com",
   adapter: vercel(),
   integrations: [sitemap()],
+  output: "server",
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve('./src'),
+        "@": path.resolve("./src"),
       },
     },
-  }
+  },
 });
