2fce0adbabc6d9dba943c491879772f46d92d1fa
midudev <miduga@gmail.com>
2025-10-21 23:22:26 +0200

    refactor(auth): standardize cookie names and improve code formatting in signout API

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,15 +1,15 @@
-import { supabase } from "@/supabase";
-import type { APIRoute } from "astro";
+import { supabase } from '@/supabase'
+import type { APIRoute } from 'astro'
 
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
-  cookies.delete("sb-access-token", { path: "/" });
-  cookies.delete("sb-refresh-token", { path: "/" });
+  cookies.delete('access-token', { path: '/' })
+  cookies.delete('refresh-token', { path: '/' })
 
   // sign out from supabase
   const { error } = await supabase.auth.signOut()
   if (error) {
-    return new Response(error.message, { status: 500 });
+    return new Response(error.message, { status: 500 })
   }
 
-  return redirect("/");
-};
+  return redirect('/')
+}
