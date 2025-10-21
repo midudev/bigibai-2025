c3a2c388b02c584ecd12e9a9499ee25138abafeb
midudev <miduga@gmail.com>
2025-10-21 23:15:01 +0200

    feat(auth): enhance cookie security settings for access and refresh tokens

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -30,9 +30,17 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
       const { access_token, refresh_token } = data.session
       cookies.set('access_token', access_token, {
         path: '/',
+        httpOnly: true,
+        secure: true, // solo HTTPS
+        sameSite: 'lax', // o 'strict'
+        maxAge: 60 * 60 * 24 * 7, // 7 días
       })
       cookies.set('refresh_token', refresh_token, {
         path: '/',
+        httpOnly: true,
+        secure: true,
+        sameSite: 'lax',
+        maxAge: 60 * 60 * 24 * 30, // 30 días
       })
       return redirect(next)
     }
